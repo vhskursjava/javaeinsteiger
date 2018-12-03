9b0668b35fa8b914eea0577447f515af32105efd
Marvin Pokroppa <marvin@tygaware.de>
2018-12-04 00:48:26 +0100

    Kursprojekt 03.12.2018

---
+++ src/Einstieg.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Einstieg {
+
+	public static void main(String[] args) {
+		
+		//Das ist ein einzeiliger Kommentar
+		
+		/*
+		 * Das
+		 * ist
+		 * ein
+		 * mehrzeiliger
+		 * Kommentar
+		 * */
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("--- Dollar in Euro Umrechner ---");
+		System.out.print("Dollar: ");
+		double dollar = Double.parseDouble(scanner.nextLine());
+		System.out.print("Kurs: ");
+		double kurs = Double.parseDouble(scanner.nextLine());
+		
+		double euro = dollar * kurs;
+		System.out.println("Euro: " + String.format("%.2f", euro));
+		
+		scanner.close();
+	}
+
+}
---
+++ src/variablen/VariablenUndDatentypen.java
@@ -0,0 +1,17 @@
+package variablen;
+
+public class VariablenUndDatentypen {
+
+	public static void main(String[] args) {
+		
+		int meineIntegerVariable;
+		meineIntegerVariable = 12;
+		
+		System.out.println(meineIntegerVariable);
+		
+		meineIntegerVariable = 1100;
+		
+		System.out.println(meineIntegerVariable);
+	}
+
+}
